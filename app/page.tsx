856d9ee1d523674217b48a243128ef4243decaca
jeenal0199 <jeenalsankhala422@gmail.com>
2026-05-10 10:45:00 +0530

    Fix homepage routing

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <iframe
+        src="/index.html"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+        }}
+      />
+    </main>
+  );
+}
